29f03045f2fe1754ae23acdc99a0038f9bb8d7f6
Jetson <None>
2019-02-02 16:38:00 -0500

    Fixing rc callback

--- ros_ws/src/rc/src/rc_manager.py
+++ ros_ws/src/rc/src/rc_manager.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python2.7
 import rospy
 import serial
+import time
 from sensor_msgs import msg
 
-def gpsCallback(joyMsg):
+ser = serial.Serial("/dev/ttyS0", 57600, timeout=1)
+
+def controllerCallback(joyMsg):
 	rospy.loginfo("X: %d", joyMsg.buttons[14])
 	rospy.loginfo("Left: %f Right: %f", joyMsg.axes[1], joyMsg.axes[3])
+	ser.write('L' + str(int(joyMsg.axes[1] * 35) + 90) + '\n');
+	ser.write('R' + str(int(joyMsg.axes[3] * 35) + 90) + '\n');
 
 
 def listener():
 	rospy.init_node('rc_listener', anonymous=True)
-	rospy.Subscriber("joy", msg.Joy, gpsCallback)
+	rospy.Subscriber("joy", msg.Joy, controllerCallback)
 	rospy.spin()
 
 if __name__ == '__main__':
-  ser = serial.Serial("ttyTHS2", 57600, timeout=1)
-  while True:
-	  ser.write("test")
-	  time.sleep(1)
-  listener()
+ 	listener()
