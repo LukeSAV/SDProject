a009170bf3404bce41bc7af4b3e774201edef271
nvidia <nvidia>
2019-04-01 13:40:44 -0400

    Fixing serial timeout and adding firebase module to launch file

--- ros_ws/src/serial/scripts/serial_manager.py
+++ ros_ws/src/serial/scripts/serial_manager.py
@@ -7,7 +7,7 @@ from std_msgs.msg import String, Bool
 from threading import Lock
 
 
-ser = serial.Serial(port="/dev/micro", baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+ser = serial.Serial(port="/dev/micro", baudrate=19200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 
 left_speed = '0'
 left_direction = '+'
@@ -21,51 +21,61 @@ controllerCallbackCalled = True
 lock = Lock()
 
 def deliveryCallback(deliveryRequestedMsg):
-	if deliveryRequestedMsg.data == True:
-		ser.write('{D}')	
-	else:
-		ser.write('{N}')
+    if deliveryRequestedMsg.data == True:
+        ser.write('{D}')	
+    else:
+        ser.write('{N}')
 
 def controllerCallback(joyMsg):
-	global left_speed
-	global right_speed
-	global left_direction
-	global right_direction
-	global mode_change
-	global prev_mode_change
-	left_speed = str(abs(int(joyMsg.axes[5] * 65)))
-	right_speed = str(abs(int(joyMsg.axes[1] * 65)))
+    global controllerCallbackCalled
+    global left_speed
+    global right_speed
+    global left_direction
+    global right_direction
+    global mode_change
+    global prev_mode_change
+    lock.acquire()
+    controllerCallbackCalled = True
+    left_speed = str(abs(int(joyMsg.axes[5] * 65)))
+    right_speed = str(abs(int(joyMsg.axes[1] * 65)))
+    lock.release()
 
-	if joyMsg.axes[5] < 0.0:
-		left_direction = '-' 
-	else:
-		left_direction = '+'
-	if joyMsg.axes[1] < 0.0 :
-		right_direction = '-' 
-	else:
-		right_direction = '+'
+    if joyMsg.axes[5] < 0.0:
+        left_direction = '-' 
+    else:
+        left_direction = '+'
+    if joyMsg.axes[1] < 0.0 :
+        right_direction = '-' 
+    else:
+        right_direction = '+'
 
-	mode_change = joyMsg.buttons[3]
-	if mode_change == 1 and prev_mode_change == 0:
-		ser.write('{M}')
-		prev_mode_change = 1
-	if mode_change == 0:
-		prev_mode_change = 0
-	lock.acquire()
-	controllerCallbackCalled = True
-	lock.release()
+    mode_change = joyMsg.buttons[3]
+
+    if mode_change == 1 and prev_mode_change == 0:
+        ser.write('{M}')
+        prev_mode_change = 1
+    if mode_change == 0:
+        prev_mode_change = 0
             
 
 def outputTimerCallback(event):
-	global controllerCallbackCalled
-	if controllerCallbackCalled == True:
-		left_speed = '0'
-		right_speed = '0'
-	ser.write('{'+ 'C' + left_direction + left_speed.zfill(3) + ' ' + right_direction + right_speed.zfill(3) + '}') #Format serial message and write it to the device on USB0
-	lock.acquire()
-	controllerCallbackCalled = False
-	lock.release()
+    global left_speed
+    global right_speed
+    ser.write('{'+ 'C' + left_direction + left_speed.zfill(3) + ' ' + right_direction + right_speed.zfill(3) + '}') #Format serial message and write it to the device on USB0
 
+def joystickTimeoutCallback(event):
+    global controllerCallbackCalled
+    global left_speed
+    global right_speed
+    if controllerCallbackCalled == False:
+        lock.acquire()
+        left_speed = str('0')
+        right_speed = str('0')
+        lock.release()
+    lock.acquire()
+    controllerCallbackCalled = False
+    lock.release()
+    
 
 """ Synchronizes to the last character of the data sent, prevents issues related to 
 incorrect serial data being sent"""
@@ -77,12 +87,13 @@ def sync():
 def listener():
     rospy.init_node('rc_listener')
     rospy.Subscriber("joy", msg.Joy, controllerCallback)
-	rospy.Subscriber("delivery_requested_status", msg.Bool, deliveryCallback)
-    rospy.Timer(rospy.Duration(0.2), outputTimerCallback)
+    rospy.Subscriber("delivery_requested_status", Bool, deliveryCallback)
+    rospy.Timer(rospy.Duration(0.1), outputTimerCallback)
+    rospy.Timer(rospy.Duration(1.0), joystickTimeoutCallback)
     enc_pub = rospy.Publisher("/encoder", String, queue_size=2)
 
-
     sync()
+
     while not rospy.core.is_shutdown():
         enc_data = ser.read(8)	 # Number of bytes from the microcontroller
         if enc_data[-1] != r'}':
