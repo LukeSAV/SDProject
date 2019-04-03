55093216a6397583c382ffe6476202257c7f3dcf
kaylwin <kaylwin@purdue.edu>
2019-04-02 20:51:23 -0400

    udpated ekf_node

--- ros_ws/src/ekf/scripts/ekf_node.py
+++ ros_ws/src/ekf/scripts/ekf_node.py
@@ -3,7 +3,9 @@ import rospy
 from sensor_msgs.msg import NavSatFix, Imu
 from std_msgs.msg import String
 from threading import Lock
-from copy import deepcopy
+from copy import deepcopy, copy
+import geopy.distance
+
 # TODO import encoder message
 
 
@@ -11,67 +13,83 @@ gps_meas_msg = NavSatFix()
 enc_meas_msg = String()
 imu_meas_msg = Imu()
 
+gps_updated = False
+enc_updated = False
+imu_updated = False
+
+# Locks so the threads don't step on eachother
 gps_lock = Lock()
 enc_lock = Lock()
 imu_lock = Lock()
 
+
+# Tick count for the encoders
+theta_l_meas = 0
+theta_r_meas = 0
+
+
 # TODO what is the default orientation?
 def gps_callback(gps_msg):
+  global gps_updated, gps_meas_msg
   gps_lock.acquire()
-  gps_meas_msg = gps_msg
+  gps_meas_msg = deepcopy(gps_msg)
+  gps_updated = True
   gps_lock.release()
 
 def imu_callback(imu_msg):
+  global imu_meas_msg, imu_updated
   imu_lock.acquire()
   imu_meas_msg = imu_msg
+  imu_updated = True
   imu_lock.release()
 
 
 def encoder_callback(encoder_msg):
-  # TODO: Set the encoder message type
-  enc_lock.acquire()
-  enc_meas_msg = encoder_msg
-  enc_lock.release()
-
-
-# This is the bulk of the program. This takes in 
-# the ekf measurements and turns them into something
-# the ekf module can handle
-def update_ekf():
-
-  # Get the locks for each of the measurments
+  global gps_updated
   gps_lock.acquire()
   gps_meas = deepcopy(gps_meas_msg)
+  gps_valid = copy(gps_updated)
+  gps_updated = False
   gps_lock.release()
 
   imu_lock.acquire()
   imu_meas = deepcopy(imu_meas_msg)
   imu_lock.release()
 
-  enc_lock.acquire()
-  enc_meas = deepcopy(enc_meas_msg)
-  enc_lock.release()
+  enc_updated = True
 
-  # TODO pass these measurements into the ekf module
+  # HELP MIKE WUT TO DO
+  # gps_meas is the gps message
+  lat_lon = (gps_meas.latitude, gps_meas.longitude)
+  purdue_fountain = ( 
 
-  pass
+  # IMU IS AVAILABLE AS imu_meas
+
+
+
+  ############# Mike takes the wheel ######
+  theta_l = int(encoder_msg.data[2:4])
+  theta_r = int(encoder_msg.data[5:7])
+
+
+
+
+  if(gps_valid):
+    print(gps_meas)
+
+
 
 
 if __name__ == "__main__":
   rospy.init_node('ekf')
 
   # TODO: Determine topic names
-  rospy.Subscriber("/imu", Imu, imu_callback) 
-  rospy.Subscriber("/gps", Imu, gps_callback)
+  rospy.Subscriber("/imu/data", Imu, imu_callback) 
+  rospy.Subscriber("/rtk_gpgga", NavSatFix , gps_callback)
   rospy.Subscriber("/encoder", String, encoder_callback)
 
-  # TODO need to set up a publisher for EKF output
+  rospy.spin()
 
-  r = rospy.Rate(10) # 10 hz
-  while not rospy.is_shutdown():
-    print("Running!")
-    update_ekf()
-    r.sleep()
 
 
 
