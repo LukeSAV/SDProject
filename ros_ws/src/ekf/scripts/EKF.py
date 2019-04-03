689985e89da41cfcdd50d95d74163b6f8aac4760
Michael Karrs <mkarrs@purdue.edu>
2019-04-03 11:10:02 -0400

    Updates to EKF

---
+++ ros_ws/src/ekf/scripts/EKF.py
@@ -0,0 +1,114 @@
+#EKF bois
+
+import numpy;
+import math;
+
+class EKF(object):
+    def __init__(self):
+        self.A = numpy.identity(5)
+        self.R = numpy.zeros((5,5))
+        self.R[0][0] = 400 # GPS covariances
+        self.R[1][1] = 400
+        self.R[2][2] = 0.01 # IMU covariance
+        self.R[3][3] = 0.04 # Encoder covariances
+        self.R[4][4] = 0.04
+        self.Q = numpy.identity(5)
+        self.Q = 0.1*self.Q
+        self.H = numpy.identity(5)
+        self.xest = numpy.zeros(5).transpose()
+        self.x = numpy.zeros(5).transpose()
+        self.Pest = numpy.identity(5)
+        self.P = numpy.identity(5)
+        self.P = 100*self.P
+        self.l = 0.5715
+
+    def update_gps_cov(NumSats):
+        #asusming x and y have the same covariance and no cross-covariance
+        self.R[0][0] = .4/NumSats
+        self.R[1][1] = .4/NumSats
+
+    def update_imu_cov(cov):
+        #assuming no cross covariance between IMU and other sensors
+        self.R[2][2] = cov
+
+    def update_encoder_cov(thetaR, thetaL):
+        #assuming left and right wheel encoders have same covariance and no cross-covariance
+        self.R[3][3] = 0.10/thetaR
+        self.R[4][4] = 0.10/thetaL
+
+    def step(self, *args):
+        if(len(args) == 6):
+            timestep, gpsX, gpsY, imuHeading, thetaR, thetaL = args
+            #Prediction Equations
+            self.A[0][3] = -0.5*math.sin(self.x[2])
+            self.A[0][4] = -0.5*math.sin(self.x[2])
+            self.A[1][3] = 0.5*math.cos(self.x[2])
+            self.A[1][4] = 0.5*math.cos(self.x[2])
+            self.A[2][3] = 1/self.l
+            self.A[2][4] = -1/self.l
+            self.xest = self.A.dot(self.x)
+            #print("Previous Vr " + str(float(self.x[3])))
+            #print("Previous Vl " + str(float(self.x[4])))
+            #print("Previous IMU Heading " + str(float(self.x[2])))
+            #print("Estimated IMU Heading" + str(float(self.xest[2])))
+            self.Pest = self.A.dot(self.P.dot(self.A.transpose())) + self.Q
+
+            #Measurement Update
+            z = numpy.array([gpsX, gpsY, imuHeading, thetaR, thetaL]).transpose()
+            self.H = numpy.identity(5)
+            self.H[0][0] = 1
+            self.H[1][1] = 1
+            self.H[2][3] = 1/(timestep*self.l)
+            self.H[2][4] = -1/(timestep*self.l)
+            self.H[3][3] = 1
+            self.H[4][4] = 1
+
+            #self.H[3][0] = 1
+            #self.H[4][1] = 1
+            #Measurement Correction
+            I = numpy.identity(5)
+            try:
+                inv = numpy.linalg.inv(self.H.dot(self.Pest.dot(self.H.transpose()))+self.R)
+            except:
+                inv = numpy.identity(5)
+            K = self.Pest.dot(self.H.transpose()).dot(inv)
+            self.x = self.xest + K.dot(z - self.H.dot(self.xest))
+            self.P = (I - K.dot(self.H)).dot(self.Pest)
+        elif (len(args) == 4):
+            timestep, imuHeading, thetaR, thetaL = args
+            # Prediction Equations
+            self.A[0][3] = -0.5 * math.sin(self.x[2])
+            self.A[0][4] = -0.5 * math.sin(self.x[2])
+            self.A[1][3] = 0.5 * math.cos(self.x[2])
+            self.A[1][4] = 0.5 * math.cos(self.x[2])
+            self.A[2][3] = 1 / self.l
+            self.A[2][4] = -1 / self.l
+            self.xest = self.A.dot(self.x)
+            # print("Previous Vr " + str(float(self.x[3])))
+            # print("Previous Vl " + str(float(self.x[4])))
+            # print("Previous IMU Heading " + str(float(self.x[2])))
+            # print("Estimated IMU Heading" + str(float(self.xest[2])))
+            self.Pest = self.A.dot(self.P.dot(self.A.transpose())) + self.Q
+
+            # Measurement Update
+            z = numpy.array([0, 0, imuHeading, thetaR, thetaL]).transpose()
+            self.H = numpy.identity(5)
+            self.H[0][0] = 0
+            self.H[1][1] = 0
+            self.H[2][3] = 1 / (timestep * self.l)
+            self.H[2][4] = -1 / (timestep * self.l)
+            self.H[3][3] = 1
+            self.H[4][4] = 1
+
+            # self.H[3][0] = 1
+            # self.H[4][1] = 1
+            # Measurement Correction
+            I = numpy.identity(5)
+            try:
+                inv = numpy.linalg.inv(self.H.dot(self.Pest.dot(self.H.transpose())) + self.R)
+            except:
+                inv = 0.1*numpy.identity(5)
+            K = self.Pest.dot(self.H.transpose()).dot(inv)
+            self.x = self.xest + K.dot(z - self.H.dot(self.xest))
+            self.P = (I - K.dot(self.H)).dot(self.Pest)
+
--- ros_ws/src/ekf/scripts/ekf_node.py
+++ ros_ws/src/ekf/scripts/ekf_node.py
@@ -4,10 +4,12 @@ from sensor_msgs.msg import NavSatFix, Imu
 from std_msgs.msg import String
 from threading import Lock
 from copy import deepcopy, copy
+from EKF import EKF
 
 # TODO import encoder message
 
 
+ekf = EKF()
 gps_meas_msg = NavSatFix()
 enc_meas_msg = String()
 imu_meas_msg = Imu()
@@ -26,13 +28,23 @@ imu_lock = Lock()
 theta_l_meas = 0
 theta_r_meas = 0
 
+# Initial gps coordinates
+init_gps_meas = NavSatFix()
+init_imu_meas = Imu()
+
+is_gps_init = False
+is_imu_init = False
+
 
 # TODO what is the default orientation?
 def gps_callback(gps_msg):
-  global gps_updated, gps_meas_msg
+  global gps_updated, gps_meas_msg, init_gps_meas
   gps_lock.acquire()
   gps_meas_msg = deepcopy(gps_msg)
   gps_updated = True
+  if(not is_gps_init):
+      init_gps_meas = deepcopy(gps_msg)
+      is_gps_init = True
   gps_lock.release()
 
 def imu_callback(imu_msg):
@@ -40,11 +52,15 @@ def imu_callback(imu_msg):
   imu_lock.acquire()
   imu_meas_msg = imu_msg
   imu_updated = True
+  if(not is_imu_init):
+      init_imu_meas = deepcopy(imu_msg)
+      is_imu_init = True
   imu_lock.release()
 
 
 def encoder_callback(encoder_msg):
   global gps_updated
+  global ekf
   gps_lock.acquire()
   gps_meas = deepcopy(gps_meas_msg)
   gps_valid = copy(gps_updated)
@@ -60,24 +76,37 @@ def encoder_callback(encoder_msg):
   # HELP MIKE WUT TO DO
   # gps_meas is the gps message
   lat_lon = (gps_meas.latitude, gps_meas.longitude)
-  purdue_fountain = ( 
+  init_lat_lon = (init_gps_meas.latitude, init_gps_longitude)
+  purdue_fountain = () # to be determined later
 
-  # IMU IS AVAILABLE AS imu_meas
+  # Convert from lat_lon to x_y
+  x_y = (lat_lon - init_lat_lon)/111139
 
-
+  # Convert IMU data into current reference frame
+  imu_x = imu_meas.orientation.x = init_imu_meas.orientation.x
 
   ############# Mike takes the wheel ######
   theta_l = int(encoder_msg.data[2:4])
-  theta_r = int(encoder_msg.data[5:7])
+  theta_r = int(encoder_msg.data[5:7])i
 
+  ekf.update_gps_cov(gps_msg.status.service)
+  ekf.update_encoder_cov(thetaR, thetaL)
 
-
+  if(gps_valid):
+      ekf.step(0.1, x_y[0], x_y[1], imu_x, thetaL, thetaR)
+  else:
+      ekf.step(0.1, imu_x, thetaL, thetaR)
+    
+  print("FILETERED OUTPUT:")
+  print("GPS X (meters): " + str(float(ekf.x[0])))
+  print("GPS Y (meters): " + str(float(ekf.x[1])))
+  print("IMU Heading (radians): " + str(float(ekf.x[2])))
+  print("Encoder Theta R (ticks): " + str(float(ekf.x[3])))
+  print("Encoder THeta L (ticks): " str(float(ekf.x[4])))
+  print("\n")
 
   if(gps_valid):
     print(gps_meas)
-
-
-
 
 if __name__ == "__main__":
   rospy.init_node('ekf')
