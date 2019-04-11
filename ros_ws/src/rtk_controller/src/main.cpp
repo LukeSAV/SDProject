cf2d1b7e534adb82c8be1bdc4857220228b30d47
Luke Armbruster <larmbru@purdue.edu>
2019-04-10 23:18:49 -0400

    Made modifications to support waypoint navigation - still need to update and send points

--- ros_ws/src/rtk_controller/src/main.cpp
+++ ros_ws/src/rtk_controller/src/main.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <exception>
+#include <iomanip>
 #include <map>
+#include <sstream>
 #include <thread>
 #include "std_msgs/String.h"
 #include "sensor_msgs/NavSatFix.h"
@@ -11,73 +13,78 @@
 #include "../include/xml_reader.h"
 #include "rtk/HeadingSpeed.h"
 
-#define ACCEPTED_DISTANCE_TO_WAYPOINT 2000
+#define ACCEPTED_DISTANCE_TO_WAYPOINT 5
+#define SERIES_LENGTH 8
 
 static ros::Publisher cmd_pub;
 static std::pair<double, double> cur_coord(0,0);
 static enum MapData::positionStatus position_status = MapData::FixNotValid;
-static int num_satellites = 0;
 static double cur_heading;
 static double cur_speed;
 static std_msgs::String pub_msg;
 
-std::chrono::time_point<std::chrono::system_clock> last_gpgga_received_time;
+static std::chrono::time_point<std::chrono::system_clock> last_ekf_received_time = std::chrono::system_clock::now(); // Last time a message was received from the EKF
+static std::chrono::time_point<std::chrono::system_clock> time_last_position_series_sent = std::chrono::system_clock::now(); // Time that the last string of points was sent to the micro
+//static std::chrono::duration<double> time_since_last_position_series_sent;
 
 static std::string next_waypoint_key = "";
 static std::string prev_waypoint_key = "";
 static MapNode prev_waypoint = MapNode(0.0f, 0.0f);
 
-static int left_speed = 90;
-static int right_speed = 90;
+static float x_series[SERIES_LENGTH] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+static float y_series[SERIES_LENGTH] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 
-void gpggaCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
+void convertPubMsg() {
+    pub_msg.data = "{K";
+    for(int i = 0; i < SERIES_LENGTH; i++) {
+        std::stringstream x_stream;
+        std::stringstream y_stream;
+        x_stream << std::fixed << std::setprecision(2) << x_series[i];
+        y_stream << std::fixed << std::setprecision(2) << y_series[i];
+        pub_msg.data += ",";
+        pub_msg.data += x_stream.str();
+        pub_msg.data += ",";
+        pub_msg.data += y_stream.str();
+    }
+    pub_msg.data += "}";
+    std::cout << pub_msg.data << std::endl;
+}
+
+void EKFCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
+    auto cur_time = std::chrono::system_clock::now();
+    std::chrono::duration<double> time_since_last_position_series_sent = cur_time - time_last_position_series_sent;
+    if(time_since_last_position_series_sent.count() < 1.0f) { // Not sending messages closer than one second apart
+        return;
+    }
+    time_last_position_series_sent = std::chrono::system_clock::now();
+
+    // Populate message
     cur_coord.first = msg->latitude;
     cur_coord.second = msg->longitude;
-    position_status = MapData::positionStatus(msg->status.status);
-    num_satellites = msg->status.service;
-    std::string closestWaypointKey = MapData::getClosestWaypoint(cur_coord);
-    last_gpgga_received_time = std::chrono::system_clock::now();
-    if(position_status == MapData::positionStatus::RTKFix || position_status == MapData::positionStatus::RTKFloat || position_status == MapData::positionStatus::DiffGPSFix) { // RTKFix/RTKFloat assuming robot position accurate to about 10cm
-        try {
-            double distance = MapData::getDistance(cur_coord, MapData::path_map.at(next_waypoint_key));
-            if(distance < ACCEPTED_DISTANCE_TO_WAYPOINT) {
-                auto temp_waypoint_key = next_waypoint_key;
-                auto next_waypoint_it = MapData::path_map.find(next_waypoint_key);
-                if(next_waypoint_it != MapData::path_map.end()) {
-                    next_waypoint_key = std::next(next_waypoint_it)->first;
-                    // Write the waypoint lat/lon to the UART pins
-                    pub_msg.data = "NEXT_WPT," + std::to_string(MapData::path_map.at(next_waypoint_key).lat) + "," + std::to_string(MapData::path_map.at(next_waypoint_key).lon);
-                    cmd_pub.publish(pub_msg);
-                }
-                else {
-                    std::cout << "end reached" << std::endl;
-                }
-                prev_waypoint_key = temp_waypoint_key;
-            }
-            MapData::linePos line_pos = MapData::getSideOfLine(MapData::path_map.at(prev_waypoint_key), MapData::path_map.at(next_waypoint_key), cur_coord);
-            double line_distance = MapData::getDistanceFromLine(MapData::path_map.at(prev_waypoint_key), MapData::path_map.at(next_waypoint_key), cur_coord);
-            //ROS_INFO("Distance from waypoint line: %lf", line_distance);
-            std::string out_msg = "Side of line: ";
-            if(line_pos == MapData::linePos::Left) {
-                out_msg += "left";
-                pub_msg.data = "Left," + std::to_string(line_distance);
-                cmd_pub.publish(pub_msg);
+
+    last_ekf_received_time = std::chrono::system_clock::now();
+
+    convertPubMsg(); // Convert the x_series and y_series points to a string to send to the microcontroller
+
+    try {
+        double distance = MapData::getDistance(cur_coord, MapData::path_map.at(next_waypoint_key)); // Check the current distance to the next waypoint
+        if(distance < ACCEPTED_DISTANCE_TO_WAYPOINT) {
+            auto temp_waypoint_key = next_waypoint_key;
+            auto next_waypoint_it = MapData::path_map.find(next_waypoint_key);
+            if(next_waypoint_it != MapData::path_map.end()) {
+                next_waypoint_key = std::next(next_waypoint_it)->first;
+                // Write the waypoint lat/lon to the UART pins
+                std::cout << "NEXT_WPT," + std::to_string(MapData::path_map.at(next_waypoint_key).lat) + "," + std::to_string(MapData::path_map.at(next_waypoint_key).lon) << std::endl;
             }
             else {
-                out_msg += "right";
-                pub_msg.data = "Right," + std::to_string(line_distance);
-                cmd_pub.publish(pub_msg);
+                std::cout << "end reached" << std::endl;
             }
-            //ROS_INFO("%s", out_msg.c_str());
-        } catch(std::out_of_range& eor) {
-            //ROS_ERROR("Waypoint out of range");
+            prev_waypoint_key = temp_waypoint_key;
         }
-    }
-    else {
-        // Connection not accurate enough. Stop robot.
-        pub_msg.data = "L90 R90";
-        cmd_pub.publish(pub_msg);
-        left_speed = right_speed = 90;
+        //MapData::linePos line_pos = MapData::getSideOfLine(MapData::path_map.at(prev_waypoint_key), MapData::path_map.at(next_waypoint_key), cur_coord);
+        //double line_distance = MapData::getDistanceFromLine(MapData::path_map.at(prev_waypoint_key), MapData::path_map.at(next_waypoint_key), cur_coord);
+    } catch(std::out_of_range& eor) {
+        ROS_ERROR("Waypoint out of range");
     }
 }
 
@@ -92,12 +99,12 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "rtk_controller_node");
 	ros::NodeHandle nh;
     cmd_pub = nh.advertise<std_msgs::String>("robot_cmd", 1000);
-    ros::Subscriber gpgga_sub = nh.subscribe("rtk_gpgga", 1000, gpggaCallback);
+    ros::Subscriber gpgga_sub = nh.subscribe("/ekf/filtered", 1000, EKFCallback);
     ros::Subscriber gpvtg_sub = nh.subscribe("rtk_gpvtg", 1000, gpvtgCallback);
     auto start_landmark = std::chrono::system_clock::now();
     auto cur_time = start_landmark;
     std::chrono::duration<double> elapsed_time;
-    xml_reader("/home/luke/SDProject/ros_ws/src/rtk_controller/purdue_mapv1.0.xml");
+    xml_reader("/home/luke/SDProject/purdue_mapv1.0.xml");
     //xml_reader("/home/ubuntu/SDProject/ros_ws/src/rtk_controller/lukes_test_path.xml");
 
     // Initialize frst waypoint
@@ -108,17 +115,14 @@ int main(int argc, char **argv)
     pub_msg.data = "NEXT_WPT," + std::to_string(MapData::path_map.at(next_waypoint_key).lat) + "," + std::to_string(MapData::path_map.at(next_waypoint_key).lon);
     cmd_pub.publish(pub_msg);
 
+    // Poll for EKF messages and publish path points
     ros::Rate r(10);
-    last_gpgga_received_time = std::chrono::system_clock::now();
+    last_ekf_received_time = std::chrono::system_clock::now();
     while(ros::ok()) {
         cur_time = std::chrono::system_clock::now();
-        elapsed_time = cur_time - last_gpgga_received_time;
-        if(elapsed_time.count() > 3.0f) { // Went 3 seconds without a GPS message. Better stop.
-            std_msgs::String pub_msg;
-            pub_msg.data = "L90 R90";
-            cmd_pub.publish(pub_msg);
-            left_speed = right_speed = 90;
-            ROS_ERROR("3 seconds without GPS data");
+        elapsed_time = cur_time - last_ekf_received_time;
+        if(elapsed_time.count() > 3.0f) { // Went 3 seconds without an EKF message. Better stop.
+            ROS_ERROR("3 seconds without EKF data");
         }
         /*elapsed_time = cur_time - start_landmark;
         if(elapsed_time.count() > 5.0f) { // Check the nearest landmark every 5 seconds
